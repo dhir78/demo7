963524a516a0a0c3fbd4c16cf03a29bea8b38321
dhir78 <65754703+dhir78@users.noreply.github.com>
2020-10-29 12:14:10 +0530

    Update scriptd.sh

--- scriptd.sh
+++ scriptd.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo su omsagent -c 'python /opt/microsoft/omsconfig/Scripts/OMS_MetaConfigHelper.py --disable'
-
 WorkspaceID=$1
 WorkspacePrimaryKey=$2
 sudo wget https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/DataConnectors/CEF/cef_installer.py&&sudo python cef_installer.py [$WorkspaceID] [$WorkspacePrimaryKey]
